dc34e23df56989c35bd546482272ffebed99d84b
MilanMareels <milan.mareels@gmail.com>
2025-04-10 20:12:45 +0200

    fix build step 2

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,11 @@ import { UsersController } from './users/users.controller';
 import { UsersService } from './users/users.service';
 import { CustomersController } from './customers/customers.controller';
 import { CustomersService } from './customers/customers.service';
+import { UserModule } from './users/users.module';
+import { CustomersModule } from './customers/customers.module';
 
 @Module({
-  imports: [],
+  imports: [UserModule, CustomersModule],
   controllers: [AppController, UsersController, CustomersController],
   providers: [AppService, UsersService, CustomersService],
 })
