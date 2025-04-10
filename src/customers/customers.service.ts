96d1951fd675f003c25c845d6ec908e924690cb3
MilanMareels <milan.mareels@gmail.com>
2025-04-10 20:37:49 +0200

    change in files

--- src/customers/customers.service.ts
+++ src/customers/customers.service.ts
@@ -3,42 +3,44 @@ import {
   Injectable,
   NotFoundException,
 } from '@nestjs/common';
-import { doesUserExist } from 'src/common/common';
-import { queryAddCustomer } from 'src/database/Customers/queryAddCustomer';
-import { queryAllCustomersForUser } from 'src/database/Customers/queryAllCustomersForUser';
-import { queryCustomersById } from 'src/database/Customers/queryCustomerById';
-import { queryCustomerByInfo } from 'src/database/Customers/queryCustomerByInfo';
-import { queryDeleteCustomersById } from 'src/database/Customers/queryDeleteCustomerById';
-import { queryUpdateCustomersById } from 'src/database/Customers/queryUpdateCustomerById';
+import 'dotenv/config';
+import { MongoClient } from 'mongodb';
 import { Customer } from 'src/types/Customer';
 import { CreateCustomerDTO } from 'src/types/DTO/CreateCustomerDTO';
 import { UpdateCustomerDTO } from 'src/types/DTO/UpdateCustomerDTO';
 import { RetrunObjectCustomers } from 'src/types/ReturnObjectCursomers';
+import { User } from 'src/types/User';
 import { v7 as uuidv7 } from 'uuid';
+
+const itemsPerPage: number = 10;
+const uri: string = process.env.MONGODB_URI!;
+const database: string = 'LannieConnect'; // Naar .env file!
+const client = new MongoClient(uri);
 
 @Injectable()
 export class CustomersService {
   async getAllCustomersForUser(userId: string, page: number) {
-    await doesUserExist(userId);
-    const customers: RetrunObjectCustomers = await queryAllCustomersForUser(
-      userId,
-      page,
-    );
+    await this.doesUserExist(userId);
+    const customers: RetrunObjectCustomers =
+      await this.queryAllCustomersForUser(userId, page);
     if (customers.customers.length === 0)
       throw new NotFoundException('Je hebt nog geen customers!');
     return customers;
   }
 
   async getCustomerById(customerId: string, userId: string) {
-    await doesUserExist(userId);
-    const customer: Customer = await queryCustomersById(customerId, userId);
+    await this.doesUserExist(userId);
+    const customer: Customer = await this.queryCustomersById(
+      customerId,
+      userId,
+    );
     if (!customer) throw new NotFoundException('Customer not found!');
     return customer;
   }
 
   async deleteCustomerById(customerId: string, userId: string) {
     await this.getCustomerById(customerId, userId);
-    await queryDeleteCustomersById(customerId, userId);
+    await this.queryDeleteCustomersById(customerId, userId);
     return { succes: true, message: 'Customer verwijdert!' };
   }
 
@@ -47,7 +49,7 @@ export class CustomersService {
       updateCustomerDTO.customerId,
       updateCustomerDTO.userId,
     );
-    return await queryUpdateCustomersById(updateCustomerDTO);
+    return await this.queryUpdateCustomersById(updateCustomerDTO);
   }
 
   async createCustomer(createCustomerDTO: CreateCustomerDTO) {
@@ -69,7 +71,7 @@ export class CustomersService {
       website: createCustomerDTO.website,
       notes: createCustomerDTO.notes,
     };
-    return await queryAddCustomer(newCustomer);
+    return await this.queryAddCustomer(newCustomer);
   }
 
   //Helpers
@@ -78,8 +80,124 @@ export class CustomersService {
     email: string,
     phone: string,
   ) {
-    const customer = await queryCustomerByInfo(companyName, email, phone);
+    const customer = await this.queryCustomerByInfo(companyName, email, phone);
     if (customer !== null)
       throw new ConflictException('Deze klant bestaat al!');
   }
+
+  // Checks
+  doesUserExist = async (userId: string) => {
+    const user = await this.queryUserByUserId(userId);
+    if (!user) {
+      throw new NotFoundException('User not found!');
+    }
+  };
+
+  // DataBase
+  queryUserByUserId = async (userId: string): Promise<User | unknown> => {
+    try {
+      return await client
+        .db(database)
+        .collection('Users')
+        .findOne({ userId: userId });
+    } catch (error) {
+      return error;
+    }
+  };
+
+  queryAddCustomer = async (newCustomer: Customer) => {
+    try {
+      return await client
+        .db(database)
+        .collection('Customers')
+        .insertOne(newCustomer);
+    } catch (error) {
+      return error;
+    }
+  };
+
+  queryAllCustomersForUser = async (userId: string, page: number) => {
+    try {
+      const customers = (await client
+        .db(database)
+        .collection('Customers')
+        .find({ userId: userId })
+        .skip((page - 1) * itemsPerPage)
+        .limit(itemsPerPage + 1)
+        .project({
+          customerId: 1,
+          userId: 1,
+          companyName: 1,
+          status: 1,
+          image: 1,
+          _id: 1,
+        })
+        .toArray()) as Customer[];
+
+      const nextPage: boolean = customers.length > itemsPerPage;
+
+      if (nextPage) customers.pop();
+
+      const returnObject: RetrunObjectCustomers = {
+        nextPage,
+        customers,
+      };
+
+      return returnObject;
+    } catch (error) {
+      return error;
+    }
+  };
+
+  queryCustomersById = async (customerId: string, userId: string) => {
+    try {
+      return await client.db(database).collection('Customers').findOne({
+        customerId: customerId,
+        userId: userId,
+      });
+    } catch (error) {
+      return error;
+    }
+  };
+
+  queryCustomerByInfo = async (
+    companyName: string,
+    email: string,
+    phone: string,
+  ) => {
+    try {
+      return await client.db(database).collection('Customers').findOne({
+        companyName: companyName,
+        email: email,
+        phone: phone,
+      });
+    } catch (error) {
+      return error;
+    }
+  };
+
+  queryDeleteCustomersById = async (customerId: string, userId: string) => {
+    try {
+      return await client.db(database).collection('Customers').deleteOne({
+        customerId: customerId,
+        userId: userId,
+      });
+    } catch (error) {
+      return error;
+    }
+  };
+
+  queryUpdateCustomersById = async (updateCustomerDTO: UpdateCustomerDTO) => {
+    try {
+      return await client.db(database).collection('Customers').updateOne(
+        {
+          customerId: updateCustomerDTO.customerId,
+          userId: updateCustomerDTO.userId,
+        },
+        { $set: updateCustomerDTO },
+      );
+    } catch (error) {
+      return error;
+    }
+  };
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,11 +1,19 @@
-import { Injectable, UnauthorizedException } from '@nestjs/common';
+import {
+  ConflictException,
+  Injectable,
+  NotFoundException,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { LoginDTO } from 'src/types/DTO/LoginDTO';
 import { User } from 'src/types/User';
 import { v7 as uuidv7 } from 'uuid';
-import { queryGetUserByEmail } from 'src/database/Users/queryUserByEmail';
-import { doesUserAlreadyExist, doesUserExist } from 'src/common/common';
 import { RegisterDTO } from 'src/types/DTO/RegisterDTO';
-import { queryAddUser } from 'src/database/Users/queryAddUser';
+import { MongoClient } from 'mongodb';
+import 'dotenv/config';
+
+const uri: string = process.env.MONGODB_URI!;
+const database: string = 'LannieConnect'; // Naar .env file!
+const client = new MongoClient(uri);
 
 @Injectable()
 export class UsersService {
@@ -13,8 +21,8 @@ export class UsersService {
 
   // Services
   async login(loginDTO: LoginDTO) {
-    const user: User = (await queryGetUserByEmail(loginDTO.email)) as User;
-    await doesUserExist(user.userId);
+    const user: User = (await this.queryGetUserByEmail(loginDTO.email)) as User;
+    await this.doesUserExist(user.userId);
     await this.comparePassword(loginDTO.password, user.password);
     return {
       user: user,
@@ -23,7 +31,7 @@ export class UsersService {
   }
 
   async register(registerDTO: RegisterDTO) {
-    await doesUserAlreadyExist(registerDTO.email);
+    await this.doesUserAlreadyExist(registerDTO.email);
     const hashPassword: string = await this.hashPassword(registerDTO.password);
     const newUser: User = {
       userId: uuidv7(),
@@ -34,7 +42,7 @@ export class UsersService {
       email: registerDTO.email,
       password: hashPassword,
     };
-    await queryAddUser(newUser);
+    await this.queryAddUser(newUser);
     return { user: newUser, succes: true };
   }
 
@@ -48,5 +56,49 @@ export class UsersService {
   hashPassword = async (password: string) => {
     const { hashSync } = await import('bcrypt-ts');
     return hashSync(password, 12);
+  };
+
+  // Checks
+  doesUserAlreadyExist = async (email: string): Promise<void> => {
+    const foundUser: User = (await this.queryGetUserByEmail(email)) as User;
+    if (foundUser !== null) throw new ConflictException('User bestaat al!');
+  };
+
+  doesUserExist = async (userId: string) => {
+    const user = await this.queryUserByUserId(userId);
+    if (!user) {
+      throw new NotFoundException('User not found!');
+    }
+  };
+
+  // DataBase
+  queryAddUser = async (newUser: User) => {
+    try {
+      await client.db(database).collection('Users').insertOne(newUser);
+    } catch (error) {
+      return error;
+    }
+  };
+
+  queryGetUserByEmail = async (email: string): Promise<User | unknown> => {
+    try {
+      return await client
+        .db(database)
+        .collection('Users')
+        .findOne({ email: email });
+    } catch (error) {
+      return error;
+    }
+  };
+
+  queryUserByUserId = async (userId: string): Promise<User | unknown> => {
+    try {
+      return await client
+        .db(database)
+        .collection('Users')
+        .findOne({ userId: userId });
+    } catch (error) {
+      return error;
+    }
   };
 }
